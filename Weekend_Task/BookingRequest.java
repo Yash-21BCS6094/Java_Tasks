96fee092a2c94f37a754a0ceaad695fd7e6cbab5
21bcs6094 <21bcs6094@cuchd.in>
2025-02-01 11:09:11 +0530

    docs: Booking_Request_Comment_Added

--- Weekend_Task/BookingRequest.java
+++ Weekend_Task/BookingRequest.java
@@ -1,17 +1,19 @@
 package Weekend_Task;
-
+// Booking request class to represent customer name and requested ticket
 public class BookingRequest {
     private String customerName = "";
     private int requestedTicket;
 
+    // Constructor to intialize customer name and requested Tickets
     public BookingRequest(String customerName, int requestedTicket){
         this.customerName = customerName;
         this.requestedTicket = requestedTicket;
     }
+    // getter for customer name
     public String getCustomerName(){
         return this.customerName;
     }
-
+    // getter for requested ticket
     public int getRequestedTicket(){
         return this.requestedTicket;
     }
